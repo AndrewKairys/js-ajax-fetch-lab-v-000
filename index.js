729a1bf0f56c02a6b346f4557939408dbd0317e8
Andrew Kairys <andrewkairys@gmail.com>
2018-10-11 13:56:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const baseURL = 'https://api.github.com';
 const user = '<YOUR_USERNAME>';
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return '';
 }
 
